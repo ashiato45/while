a855bca4459a3a0d5932305fad67f8ce4a63dd1a
ashiato45 <katiiijp@gmail.com>
2018-01-20 20:27:56 +0900

    implemented boolParser

--- app/Main.hs
+++ app/Main.hs
@@ -53,7 +53,78 @@ calcExpression (EBin a o b) =
     OSub -> (calcExpression a) - (calcExpression b)
     OMult -> (calcExpression a) * (calcExpression b)
     ODiv -> (calcExpression a) / (calcExpression b)
-  
+
+data TBoolOperator = BOAnd | BOOr
+  deriving Show
+data TCompareOperator = COEq | COLt | COLe | COGt | COGe | CONeq
+  deriving Show
+data TBool = BTrue
+           | BFalse
+           | BBin TBool TBoolOperator TBool
+           | BNot TBool
+           | BCompare TExpression TCompareOperator TExpression
+  deriving Show
+boolParser :: Parsec String st TBool
+boolParser =
+  try (do
+          spaces
+          char '!'
+          spaces
+          x <- boolParser
+          return $ BNot x
+      )
+  <|>
+  try (do
+          spaces
+          char '('
+          spaces
+          x <- boolParser
+          spaces
+          o <- oneOf "&|"
+          spaces
+          y <- boolParser
+          spaces
+          char ')'          
+          let oo = case o of
+                '&' -> BOAnd
+                '|' -> BOOr
+          return $ BBin x oo y
+          )
+  <|>
+  try (do
+          spaces
+          char '('
+          spaces
+          x <- expressionParser
+          spaces
+          o <- try (string "==")
+               <|> try (string "<=")
+               <|> try (string "<")
+               <|> try (string ">=")
+               <|> try (string ">")
+               <|> string "!="
+          spaces
+          y <- expressionParser
+          spaces
+          char ')'
+          let oo = case o of
+                "==" -> COEq
+                "<=" -> COLe
+                "<" -> COLt
+                ">=" -> COGe
+                ">" -> COGt
+                "!=" -> CONeq
+          return $ BCompare x oo y
+  )
+  <|>
+  (do
+      spaces
+      x <- (string "TRUE") <|> (string "FALSE")
+      return $ case x of
+        "TRUE" -> BTrue
+        "FALSE" -> BFalse
+  )
+           
 
 main :: IO ()
 main = someFunc
