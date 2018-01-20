ae8e3ba6b3aee4a510193c3124e9b35fb9445101
ashiato45 <katiiijp@gmail.com>
2018-01-20 17:04:36 +0900

    rewrited numberParser by do

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,14 @@
 module Main where
 
 import Lib
+import Text.Parsec
+
+type TNumber = Int
+numberParser:: Parsec String st TNumber
+-- numberParser = read <$> (many $ oneOf "0123456789")
+numberParser = do
+  x <- many $ oneOf "0123456789"
+  return $ read x
 
 main :: IO ()
 main = someFunc
